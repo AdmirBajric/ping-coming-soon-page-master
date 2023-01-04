1b4befeafcd8cc8b28fd1acc118b7084bf3d9452
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-04 23:01:00 +0100

    Update files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ping Coming Soon Page Master
+
+## Frontend Mentor
+
+### https://www.frontendmentor.io/
+
+Ping Coming Soon Page Master
+
+Technology used : HTML, CSS, Flexbox, Javascript
---
+++ main.js
@@ -0,0 +1,32 @@
+const submit = document.querySelector(".form button");
+const input = document.querySelector(".form input");
+const error = document.getElementById("error");
+
+submit.addEventListener("click", (e) => {
+  if (check(input)) {
+    alert("Thank you for Subscribing!");
+  }
+});
+
+input.addEventListener("input", (e) => {
+  check(e.target);
+});
+
+const validateEmail = (inputText) => {
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return inputText.value.match(mailFormat);
+};
+
+const check = (inputTxt) => {
+  if (!validateEmail(inputTxt)) {
+    error.style.display = "flex";
+    error.innerHTML = `<p>Please provide a valid email address</p>`;
+    inputTxt.style.border = "1px solid #ff5263";
+    return false;
+  } else {
+    error.style.display = "none";
+    error.innerHTML = ``;
+    inputTxt.style.border = "1px solid #c2d3ff";
+    return true;
+  }
+};
